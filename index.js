249084eac3e0f2ebc37d13ce9ec3cf7d197f1481
codebreaker0001 <122373416+codebreaker0001@users.noreply.github.com>
2023-04-04 21:58:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    
+
+    
+});
+var formEmail =document.getElementById("form-email")
+var formName =document.getElementById("form-name")
+var formSubject =document.getElementById("form-subject")
+var formMessage=document.getElementById("form-message")
+var formButton=document.getElementById("form-button")
+
+
+function getFormData(){
+    console.log(formEmail.value,formName.value,formSubject.value,formMessage.value);
+    window.open(`mailto:${formEmail.value}?subject=${formSubject.value}&body=${formMessage.value}`)
+}
+
+formButton.onclick((e)=>{
+    console.log("hii")
+    e.preventDefault()
+    getFormData()
+})
